7cb4fc6d510ef61eea096671f986c9add6de77c2
NIKHIL SHARMA <73777412+nikhil101001@users.noreply.github.com>
2022-12-21 01:44:09 +0530

    Update scanerscript.js

--- scanerscript.js
+++ scanerscript.js
@@ -7,7 +7,7 @@ const wrapper = document.querySelector(".wrapper"),
 
 function fetchRequest(file, formData) {
   infoText.innerText = "Scanning QR Code...";
-  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+  fetch("https://api.qrserver.com/v1/read-qr-code/", {
     method: "POST",
     body: formData,
   })
